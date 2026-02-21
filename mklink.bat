00953949c899dc78552138283c8f3b1a8daf9667
Radek Mikulka <radek.mikulka@w.aldagames.com>
2026-02-21 20:53:52 +0100

    data

--- mklink.bat
+++ mklink.bat
@@ -1,4 +1,4 @@
-mklink /J PharaohUnity\Assets\AldaEngine C:\Dev\AldaEngine7
+mklink /J PharaohUnity\Assets\CommonEngine C:\Dev\AldaEngine7
 mklink /J PharaohUnity\Assets\ServiceEngine C:\Dev\ServiceEngine2
 mklink /J MainServer\Server\Libraries\AldaEngine C:\Dev\AldaEngine7\Sharp
 mklink /J MainServer\Server\ServerData\ServerData PharaohUnity\Assets\_ServerData
